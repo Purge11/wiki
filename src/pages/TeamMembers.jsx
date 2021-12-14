e56bc06aa22d4791db3b51b959f27388905bfb26
Maybe <kubapredki1@gmail.com>
2021-12-14 17:21:13 +0100

    changes

--- src/pages/TeamMembers.jsx
+++ src/pages/TeamMembers.jsx
@@ -13,7 +13,7 @@ export default function TeamMembers() {
                                 <th scope="col">Name</th>
                                 <th scope="col ">language</th>
                                 <th scope="col">Role</th>
-                                <th scope="col">Sex</th>
+                                
                             </tr>
                         </thead>
                         <tbody>
@@ -21,19 +21,19 @@ export default function TeamMembers() {
                                 <th scope="row danger">Purge</th>
                                 <td>TR/ENG</td>
                                 <td>Owner</td>
-                                <td>Male</td>
+                                
                             </tr>
                             <tr className='tr1'>
                                 <th scope="row">Oge</th>
                                 <td>TR/ENG</td>
                                 <td>Game Designer</td>
-                                <td>Male</td>
+                               
                             </tr>
                             <tr className='tr1'>
                                 <th scope="row">Revox</th>
                                 <td>PL/ENG</td>
                                 <td>Client Modder</td>
-                                <td>Male</td>
+                               
                             </tr>
                             <tr className='tr1'>
                                 <th scope="row">SvC11</th>
@@ -45,73 +45,73 @@ export default function TeamMembers() {
                                 <th scope="row">Decameron</th>
                                 <td>TR/ENG</td>
                                 <td>Team Manager</td>
-                                <td>Male</td>
+                             
                             </tr>
                             <tr className='tr3'>
                                 <th scope="row">Killrog</th>
                                 <td>PL/ENG</td>
                                 <td>Web Developer</td>
-                                <td>Male</td>
+                              
                             </tr>
                             <tr className='tr9'>
                                 <th scope="row">Nyx</th>
                                 <td>PL/ENG</td>
                                 <td>Super Game Master</td>
-                                <td>Male</td>
+                             
                             </tr>
                             <tr className='tr4'>
                                 <th scope="row">Ryora</th>
                                 <td>TR/ENG</td>
                                 <td>Game Master</td>
-                                <td>Male</td>
+                                
                             </tr>
                             <tr className='tr4'>
                                 <th scope="row">Sigma</th>
                                 <td>JAP/ENG</td>
                                 <td>Game Master</td>
-                                <td>Male</td>
+                             
                             </tr>
                             <tr className='tr4'>
                                 <th scope="row">Akita</th>
                                 <td>HK/ENG</td>
                                 <td>Game Master</td>
-                                <td>Male</td>
+                                
                             </tr>
                             <tr className='tr8'>
                                 <th scope="row">RevengeKai</th>
                                 <td>ESP/ENG</td>
                                 <td>Trial Game Master</td>
-                                <td>Male</td>
+                  
                             </tr>
                             <tr className='tr6'>
                                 <th scope="row">Nayla</th>
                                 <td>DE/ENG</td>
                                 <td>Head of Game Supporter</td>
-                                <td>Female</td>
+                               
                             </tr>
                             <tr className='tr5'>
                                 <th scope="row">Dark Legend</th>
                                 <td>ITA/ENG</td>
                                 <td>Event Manager</td>
-                                <td>Male</td>
+                                
                             </tr>
                             <tr className='tr7'>
                                 <th scope="row">Ilya</th>
                                 <td>FR/ENG</td>
                                 <td>Game Supporter</td>
-                                <td>Female</td>
+                               
                             </tr>
                             <tr className='tr7'>
                                 <th scope="row">MMAA</th>
                                 <td>ITA/ENG</td>
                                 <td>Game Supporter</td>
-                                <td>Male</td>
+                             
                             </tr>
                             <tr className='tr7'>
                                 <th scope="row">Starwick</th>
                                 <td>DE/ENG</td>
                                 <td>Game Supporter</td>
-                                <td>Male</td>
+                             
                             </tr>
                         </tbody>
                     </table>
