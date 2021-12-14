3a3025e8b4873c72af0411df860b4fabc217228b
Maybe <kubapredki1@gmail.com>
2021-12-14 13:00:45 +0100

    hash router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import HashRouter from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
