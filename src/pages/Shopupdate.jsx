02dbaf4622d570e02541dbc0b8a16fe65925eee6
Purge <65049931+Purge11@users.noreply.github.com>
2022-01-07 01:51:02 +0300

    Update Shopupdate.jsx

--- src/pages/Shopupdate.jsx
+++ src/pages/Shopupdate.jsx
@@ -8,7 +8,7 @@ export default function Shopupdate() {
                     Hello NosVoid players,
                     <br/>
                     <br/>
-                    New Fortune Wheel update is alive. [6.021.202]
+                    New Fortune Wheel update is alive. [6.01.2022]
                     <br/>
                     <br/>
                     <h3>What can i get from the Fortune Wheel?</h3>
