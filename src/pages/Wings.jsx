8b29f6dff2761c5a30ebb5af8b3bcd738908bc6b
Maybe <kubapredki1@gmail.com>
2022-01-11 20:38:03 +0100

    wings

--- src/pages/Wings.jsx
+++ src/pages/Wings.jsx
@@ -3,13 +3,422 @@ import React from 'react'
 export default function Wings() {
     return (
         <div className='wings'>
-            <div class="container">
-                <div class="row  ">
+            <div class="">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/1.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Angel Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Low Tier Wings NPC. [Prestige 1 Map.]</h5>
 
-                   
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
                 </div>
+
             </div>
-            
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/2.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Devil Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Low Tier Wings NPC. [Prestige 1 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/3.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Titan Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Wings NPC. [Prestige 2 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/4.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Dracula Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Rainbow Battle Exchange NPC.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/5.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Void Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Instant Combat NPC.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/6.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Petal Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Void Mall.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/18.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Zephyr Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Zephyr Wings Trade NPC and Void Mall. [Act4]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/7.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Golden Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in High Tier Wings NPC. [Prestige 5 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/8.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Lightning Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Talent Arena NPC and Void Mall.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/9.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Ice Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Wings NPC. [Prestige 2 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/10.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Blade Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Caligor Boxes and Void Mall.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/11.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Fire Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Wings NPC. [Prestige 2 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/12.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Blazing Fire Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Wings NPC. [Prestige 3 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/13.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Archdaemon Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Prestige Reward Boxes.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/14.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Onyx Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Onyx Wings Trade NPC and Void Mall. [Act4]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/15.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Frosty Ice Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Wings NPC. [Prestige 3 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/16.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Crystal Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained from Act4 Craft NPC and Void Mall.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="mt-3">
+                <div class="card-group   ">
+                    <div class="card">
+                        <div class="flip-card-inner2 ">
+                            <div class="flip-card-front">
+                                <img src="asset/wings/17.png" class="card-img-top mx-auto pt-1 " alt="..."/>
+                                <div class="card-body ">
+                                    <h5 class="card-title nosvoid-color ">Titan Wings</h5>
+                                    <p class="card-text ">
+                                        <h5>Can be obtained in Wings NPC. [Prestige 2 Map.]</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
+
     )
 }
