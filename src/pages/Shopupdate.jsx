2d3492e9cce92ad8e0b262f6845491c3af5b84a5
Maybe <kubapredki1@gmail.com>
2022-01-17 15:23:50 +0100

    shop update

--- src/pages/Shopupdate.jsx
+++ src/pages/Shopupdate.jsx
@@ -13,210 +13,203 @@ export default function Shopupdate() {
                     <br/>
                     <h3>What can i get from the Fortune Wheel?</h3>
                     <br/>
-                
+
+                    <div className='mt-5'></div>
                     <div className='mt-5'>
-               
-                    </div>
-                    <div className='mt-5'>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">
-                                Xmas Random Wheel Box</th>
-                                <th scope="col"><img src="asset/shop/1.webp" alt=""/></th>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">
+                                        Xmas Random Wheel Box</th>
+                                    <th scope="col"><img src="asset/shop/1.webp" alt=""/></th>
 
-                            </tr>
-                            <d class="item-effect-shell">
-                            - Hongbi Partner Card
-                            <br/>
-- Maru Partner Card
-<br/>
-- Amora Partner Card
-<br/>
-- Beach Costume Set
-<br/>
-- Pumpkin Witch Costume Set
-<br/>
-- Simp Title
-<br/>
-- Change Skin 2
-<br/>
-- Pirate Specialist
-<br/>
-- Mini Pete</d>
+                                </tr>
+                                <d class="item-effect-shell">
+                                    - Hongbi Partner Card
+                                    <br/>
+                                    - Maru Partner Card
+                                    <br/>
+                                    - Amora Partner Card
+                                    <br/>
+                                    - Beach Costume Set
+                                    <br/>
+                                    - Pumpkin Witch Costume Set
+                                    <br/>
+                                    - Simp Title
+                                    <br/>
+                                    - Change Skin 2
+                                    <br/>
+                                    - Pirate Specialist
+                                    <br/>
+                                    - Mini Pete</d>
 
-                            <br/>
+                                <br/>
 
-                        </thead>
-</table>
-                    </div>
-                    <div className='mt-5'>
-                    <table class="table">
-                     <thead>
-                            <tr>
-                                <th scope="col">Dark Knight's Mask</th>
-                                <th scope="col"><img src="asset/shop/2.webp" alt=""/></th>
-
-                            </tr>
-                            <d class="item-effect-shell">
-                            - Damage to all monsters is increased by 5%.</d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Damage in Raids is increased by 5%.</d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Increases Gold earned by 3%.</d>
-                            
-                           
-
-                        </thead>
+                            </thead>
                         </table>
                     </div>
                     <div className='mt-5'>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Dark Knight's Helmet</th>
-                                <th scope="col"><img src="asset/shop/3.webp" alt=""/></th>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Dark Knight's Mask</th>
+                                    <th scope="col"><img src="asset/shop/2.webp" alt=""/></th>
 
-                            </tr>
-                            <d class="item-effect-shell">
-                                - Damage in Raids is increased by 5%.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - All elemental resistance is increased by 3.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                                - The chance of receiving a critical hit is increased by 1%. 
-                            </d>
-                            <br/>
+                                </tr>
+                                <d class="item-effect-shell">
+                                    - Damage to all monsters is increased by 5%.</d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Damage in Raids is increased by 5%.</d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Increases Gold earned by 3%.</d>
 
-                        </thead>
-</table>
+                            </thead>
+                        </table>
                     </div>
                     <div className='mt-5'>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Chloe (S Buff)</th>
-                                <th scope="col"><img src="asset/shop/4.webp" alt=""/></th>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Dark Knight's Helmet</th>
+                                    <th scope="col"><img src="asset/shop/3.webp" alt=""/></th>
 
-                            </tr>
-                            <d class="item-effect-shell">
-                                - Experience gain is increased by 20%.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                                - Increases champion experience received by 20%.
-                            </d>
-                            <br/>
-                            
-                            <d class="item-effect-shell">
-                                - Increases Gold earned by 15%.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Movement speed is increased by 1.
-                            </d>
-                            <br/>
+                                </tr>
+                                <d class="item-effect-shell">
+                                    - Damage in Raids is increased by 5%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - All elemental resistance is increased by 3.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - The chance of receiving a critical hit is increased by 1%.
+                                </d>
+                                <br/>
 
-                        </thead>
-</table>
+                            </thead>
+                        </table>
                     </div>
                     <div className='mt-5'>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Venus (S Buff)</th>
-                                <th scope="col"><img src="asset/shop/5.webp" alt=""/></th>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Chloe (S Buff)</th>
+                                    <th scope="col"><img src="asset/shop/4.webp" alt=""/></th>
 
-                            </tr>
-                            <d class="item-effect-shell">
-                            - When raid is finished, there's a 5% chance that box drops second time.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Damage in Raids is increased by 10%.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Increases champion experience received by 15%.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Movement speed is increased by 1.
-                            </d>
-                            
+                                </tr>
+                                <d class="item-effect-shell">
+                                    - Experience gain is increased by 20%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Increases champion experience received by 20%.
+                                </d>
+                                <br/>
 
-                        </thead>
-</table>
+                                <d class="item-effect-shell">
+                                    - Increases Gold earned by 15%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Movement speed is increased by 1.
+                                </d>
+                                <br/>
+
+                            </thead>
+                        </table>
                     </div>
                     <div className='mt-5'>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Frost Warrior Costume Set</th>
-                                <th scope="col"><img src="asset/shop/6.webp" alt=""/></th>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Venus (S Buff)</th>
+                                    <th scope="col"><img src="asset/shop/5.webp" alt=""/></th>
 
-                            </tr>
-                            <br/>
-                            Hat:
-                            <hr/>
-                            <d class="item-effect-shell">
-                                - All attacks are increased by 4%.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Increases damage from critical hits by 10%.
-                            </d>
-                           
-                          
-                            <br/>
-                            <br/>
-                            Costume:
-                            <hr/>
-                            <d class="item-effect-shell">
-                            - All defences are increased by 4%.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Dodge is increased by 50.
-                            </d>
-                            <br/>
-                            <br/>
-                            Costume Wings:
-                            <hr/>
-                            <d class="item-effect-shell">
-                            - Shadow element is increased by 30.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - Movement speed is increased by 1.
-                            </d>
-                            <br/>
-                            <d class="item-effect-shell">
-                            - There is a 1% chance of causing Shivering Frost.
-                            </d>
-                            <br/>
-                           
-                            <hr/>
-                            <d class="text-danger">
-                            Shivering Frost Debuff (4 seconds) : 
-                            <br/>
-- Movement speed is decreased by 5.
-<br/>
-- No attack possible
-<img src="asset/shop/7.png" alt=""/>   
-                           
-                            </d>
-                           
+                                </tr>
+                                <d class="item-effect-shell">
+                                    - When raid is finished, there's a 5% chance that box drops second time.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Damage in Raids is increased by 10%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Increases champion experience received by 15%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Movement speed is increased by 1.
+                                </d>
 
-                        </thead>
-   </table>                    
+                            </thead>
+                        </table>
                     </div>
-                    
+                    <div className='mt-5'>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Frost Warrior Costume Set</th>
+                                    <th scope="col"><img src="asset/shop/6.webp" alt=""/></th>
+
+                                </tr>
+                                <br/>
+                                Hat:
+                                <hr/>
+                                <d class="item-effect-shell">
+                                    - All attacks are increased by 4%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Increases damage from critical hits by 10%.
+                                </d>
+
+                                <br/>
+                                <br/>
+                                Costume:
+                                <hr/>
+                                <d class="item-effect-shell">
+                                    - All defences are increased by 4%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Dodge is increased by 50.
+                                </d>
+                                <br/>
+                                <br/>
+                                Costume Wings:
+                                <hr/>
+                                <d class="item-effect-shell">
+                                    - Shadow element is increased by 30.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Movement speed is increased by 1.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - There is a 1% chance of causing Shivering Frost.
+                                </d>
+                                <br/>
+
+                                <hr/>
+                                <d class="text-danger">
+                                    Shivering Frost Debuff (4 seconds) :
+                                    <br/>
+                                    - Movement speed is decreased by 5.
+                                    <br/>
+                                    - No attack possible
+                                    <img src="asset/shop/7.png" alt=""/>
+
+                                </d>
+
+                            </thead>
+                        </table>
+                    </div>
+
                 </div>
             </div>
 
