42a1ef72ffb3209abe9e53f23f37fe39688a10ec
Maybe <kubapredki1@gmail.com>
2022-01-31 15:05:13 +0100

    update

--- src/pages/Shopupdate.jsx
+++ src/pages/Shopupdate.jsx
@@ -8,7 +8,7 @@ export default function Shopupdate() {
                     Hello NosVoid players,
                     <br/>
                     <br/>
-                    New Fortune Wheel update is alive. [25.01.2022]
+                    New Fortune Wheel update is alive. [30.01.2022]
                     <br/>
                     <br/>
                     <h3>What can i get from the Fortune Wheel?</h3>
@@ -20,22 +20,20 @@ export default function Shopupdate() {
                             <thead>
                                 <tr>
                                     <th scope="col">
-                                    Xmas Random Wheel Box 2</th>
-                                    <th scope="col"><img src="asset/shop/xmas.png" alt=""/></th>
+                                    Dark Knight's Mask</th>
+                                    <th scope="col"><img src="asset/shop/mask.png" alt=""/></th>
 
                                 </tr>
                                 <d class="item-effect-shell">
-                                - Nelia Nymph Partner Card
-                                    <br/>
-                                    - Frankestein Partner Card
-                                    <br/>
-                                    - Partner Skill Ticket (Single)
-                                    <br/>
-                                    - Pirate Title
-                                    <br/>
-                                    - Pumpkin Bushtail Pet
-                                    </d>
-
+                                    - Damage to all monsters is increased by 5%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - Damage in Raids is increased by 5%. 
+                                </d>
+                                <d class="item-effect-shell">
+                                    - Increases Gold earned by 3%.
+                                </d>
                                 <br/>
 
                             </thead>
@@ -45,13 +43,21 @@ export default function Shopupdate() {
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Betting Costume</th>
-                                    <th scope="col"><img src="asset/shop/beting.webp" alt=""/></th>
+                                    <th scope="col">Dark Knight's Helmet</th>
+                                    <th scope="col"><img src="asset/shop/helmet.png" alt=""/></th>
 
                                 </tr>
                                 <d class="item-effect-shell">
-                                    - There's 10% more of getting rarity 7-8 while betting.</d>
+                                    - Damage in Raids is increased by 5%.
+                                </d>
                                 <br/>
+                                <d class="item-effect-shell">
+                                    - All elemental resistance is increased by 3.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                    - The chance of receiving a critical hit is increased by 1%.
+                                </d>
                                 
 
                             </thead>
@@ -61,113 +67,95 @@ export default function Shopupdate() {
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Sugar Mommy Title</th>
-                                    <th scope="col"><img src="asset/shop/mommy.webp" alt=""/></th>
+                                    <th scope="col">Venus (S Buff)</th>
+                                    <th scope="col"><img src="asset/shop/venus.png" alt=""/></th>
 
                                 </tr>
                                 <d class="item-effect-shell">
-                                - Increased chance of getting a nude by hot milf by 99%. (Aesthetic)
-                                </d>
-                                <br/>
-                               
-
-                            </thead>
-                        </table>
-                    </div>
-                    <div className='mt-5'>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">King of Void Title</th>
-                                    <th scope="col"><img src="asset/shop/kov.webp" alt=""/></th>
-
-                                </tr>
-                                <d class="item-effect-shell">
-                                    - All attacks are increased by 5%.
+                                - When raid is finished, there's a 5% chance that box drops second time.
                                 </d>
                                 <br/>
                                 <d class="item-effect-shell">
-                                    - All defences are increased by 5%.
-                                </d>
-                               
-
-                            </thead>
-                        </table>
-                    </div>
-                    <div className='mt-5'>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Ceremonial Helmet</th>
-                                    <th scope="col"><img src="asset/shop/ceremonial.webp" alt=""/></th>
-
-                                </tr>
-                                <d class="item-effect-shell">
-                                - Up to 4 level there is a 15% chance of never getting a bad effect.
+                                - Damage in Raids is increased by 10%.
                                 </d>
                                 <br/>
                                 <d class="item-effect-shell">
-                                - Increases PvP attack power by 10%.
-                                </d>
-                                <br/>
-                                <d class="item-effect-shell">
-                                - Reduces damage received in PvP by 5%.
-                                </d>
-                               
-
-                            </thead>
-                        </table>
-                    </div>
-                    <div className='mt-5'>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Death Lancer Costume Set</th>
-                                    <th scope="col"><img src="asset/shop/lancer.webp" alt=""/></th>
-
-                                </tr>
-                                <br/>
-                                Hat:
-                                <hr/>
-                                <d class="item-effect-shell">
-                                    - All attacks are increased by 5%.
-                                </d>
-                                <br/>
-                                <d class="item-effect-shell">
-                                    - All element energies are increased by 75.
-                                </d>
-                                <br/>
-                                <d class="item-effect-shell">
-                                - Maximum HP is increased by 1000.
-                                </d>
-
-                                <br/>
-                                <br/>
-                                Costume:
-                                <hr/>
-                                <d class="item-effect-shell">
-                                - All defences are increased by 5%.
-                                </d>
-                                <br/>
-                                <d class="item-effect-shell">
-                                - Up to level 4 there is a 7% chance of never getting a bad effect.
+                                - Increases champion experience received by 15%.
                                 </d>
                                 <br/>
                                 <d class="item-effect-shell">
                                 - Movement speed is increased by 1.
                                 </d>
-                                <br/>
-                                <br/>
-                                Costume Wings:
-                                <hr/>
+
+                            </thead>
+                        </table>
+                    </div>
+                    <div className='mt-5'>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Aegir (S Buff)</th>
+                                    <th scope="col"><img src="asset/shop/aegir.webp" alt=""/></th>
+
+                                </tr>
                                 <d class="item-effect-shell">
-                                - All defences are increased by 2%.
+                                - Meele attacks are increased by 8%.
                                 </d>
                                 <br/>
                                 <d class="item-effect-shell">
-                                - Damage from critical hits is reduced by 10%.
+                                - All defence powers are increased by 150.
                                 </d>
                                 <br/>
+                                <d class="item-effect-shell">
+                                - Increases damage in Christmas Raid by 5%.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                - There is a 1% chance of causing Severe Electric Shock.
+                                </d>
+                               
+
+                            </thead>
+                        </table>
+                    </div>
+                    <div className='mt-5'>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Daniel Ducat (S Buff)</th>
+                                    <th scope="col"><img src="asset/shop/daniel.webp" alt=""/></th>
+
+                                </tr>
+                                <d class="item-effect-shell">
+                                - There's a 20% chance of catching a bigger fish.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                - There's 30% chance to not consume a fish bait.
+                                </d>
+                                <br/>
+                                <d class="item-effect-shell">
+                                - The fishing experience points you receive are increased by 12%.
+                                </d>
+                               
+
+                            </thead>
+                        </table>
+                    </div>
+                    <div className='mt-5'>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Betting Necklace</th>
+                                    <th scope="col"><img src="asset/shop/betingn.webp" alt=""/></th>
+
+                                </tr>
+                              
+                                
+                                <d class="item-effect-shell">
+                                - There's 7% more chance of getting rarity 7-8 while betting.
+                                </d>
+                                
                                 
 
                                
