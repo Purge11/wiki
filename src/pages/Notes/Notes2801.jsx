20d052b3b501a6d49c9891ff93b878d8e82217d6
Blauker <giyuudev.contact@gmail.com>
2022-02-05 19:16:23 +0100

    Updated General Changes [05.02.2022]

--- src/pages/Notes/Notes2801.jsx
+++ src/pages/Notes/Notes2801.jsx
@@ -5,22 +5,42 @@ export default function Notes2801() {
         <div className='notes'>
             <div class="container">
                 <div class="row mt-5">
-                <ul class="list-group">
-                    <h5>General Changes 29.01.2022</h5>
-  <li class="list-group-item mt-3">- Fixed an issue with Act4 Raids where you couldn't get rewards, boss or raid portal sometimes.</li>
-  <li class="list-group-item">- Fixed an visual issue while selling Gold Coins to NPC.</li>
-  <li class="list-group-item">- Removed the base map of Act4 Raids, you will start in boss map from now on.</li>
-  <li class="list-group-item">- Added the '' $AddOldTeam '' command, which will invite to raid everyone from previous finished raid.</li>
-  <li class="list-group-item">- The family buffs are disabled til end of PvP Tournament.</li>
-  <li class="list-group-item">- Added the MVP Boxes for best damage in raid for every class. (That system will work if there is more than 10 people in the raid team.)   <br/> The MVP Boxes will work for following raids;
-Draco, Glacerus, Laurena, Zenas, Erenia, Fernon, Kirollas, Pure Beast, Belial and Paimon Raids. <br/> 
-</li>
-<li class="list-group-item"><img src="asset/notes/MVPBoxes.png" class="float-none" alt=""/></li>
-</ul>
-               
+                    <ul class="list-group">
+                        <h4>General Changes 05.02.2022</h4>
+                        <br/>
+                        <h5><d class="nosvoid-color">#Fixes</d></h5>
+                        <li class="list-group-item">Fixed the bug with Slight Freeze debuff on Frozen Swordfish, Coral Wand and Lobster Claw weapon skins.</li>
+                        <li class="list-group-item">Fixed an visual issue with the Prestige 8 Skins, that it showed skins are only for archer.</li>
+                        <li class="list-group-item">Fixed the bug with C60 Equipment scrolls, that scroll didn't disappear after using it.</li>
+                        <li class="list-group-item">Fixed the '' Reduces the enemy's elemental resistances '' buff on the Honeybee Crown.</li>
+                        <li class="list-group-item">Fixed an issue with Broken Yertirand Wand material, that it wasn't tradeable.</li>
+                        <li class="list-group-item">Fixed an issue with Broken Heart of Belial, that it wasn't tradeable.</li>
+                        <li class="list-group-item">Fixed an visual bug with '' Damage in Raid '' bonus on C60 Weapons.</li>
+                        <li class="list-group-item">Fixed an visual bug with Tower's Metal material.</li>
+                        <li class="list-group-item">Fixed wrong buff type on Shadow King's Helmet.</li>
+
+                        <br/>
+                        <h5><d class="nosvoid-color">#Changes</d></h5>
+                        <li class="list-group-item">Changed stuff inside Hardcore Coins Box, instead of getting all coins at the same time you will get 
+                                                    boxes called '' EM, MH, HVH, VHE Coins Box '' 2 different coins inside one box.</li>
+                        <li class="list-group-item">Increased Reputation amount from 2.7k to 36k in Hardcore Raid.</li>
+                        <li class="list-group-item">Decreased stun of 10-30 Hero Map mobs to 2%.</li>
+                        <li class="list-group-item">Removed stuns from 50-60 Hero Map mobs.</li>
+                        <li class="list-group-item">Nerfed reduce ranged damage of the raid bosses:<br/>
+                                                    <i>[Glacerus         70% > 50%]<br/>
+                                                    [Laurena          65% > 50%]<br/>
+                                                    [Zenas            70% > 55%]<br/>
+                                                    [Erenia           80% > 50%]<br/>
+                                                    [Kirollas         70% > 50%]<br/>
+                                                    [Belial           80% > 65%]<br/>
+                                                    [Paimon           80% > 65%]</i></li>
+                        
+                        <br/>
+                        <h5><d class="nosvoid-color">#News</d></h5>
+                        <li class="list-group-item">Added new PvE Skin Upgrades. [Can be upgraded to max 5]</li>
+                    </ul>
                 </div>
             </div>
-           
         </div>
     )
 }
