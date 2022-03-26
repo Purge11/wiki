0c31f1d939f1516e8b998620ec13f1f6fb9e5d98
Blauker <giyuudev.contact@gmail.com>
2022-03-26 03:54:03 +0100

    Wings

--- src/pages/Wings.jsx
+++ src/pages/Wings.jsx
@@ -8,7 +8,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2">
                             <div className="flip-card-front">
-                                <img src="asset/wings/1.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/1.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Angel Wings</h5>
                                     <p className="card-text ">
@@ -31,7 +31,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/2.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/2.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Devil Wings</h5>
                                     <p className="card-text ">
@@ -54,7 +54,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/3.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/3.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Titan Wings</h5>
                                     <p className="card-text ">
@@ -77,7 +77,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/4.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/4.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Dracula Wings</h5>
                                     <p className="card-text ">
@@ -100,7 +100,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/5.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/5.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Void Wings</h5>
                                     <p className="card-text ">
@@ -123,7 +123,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/6.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/6.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Petal Wings</h5>
                                     <p className="card-text ">
@@ -146,7 +146,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/18.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/18.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Zephyr Wings</h5>
                                     <p className="card-text ">
@@ -169,7 +169,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/7.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/7.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Golden Wings</h5>
                                     <p className="card-text ">
@@ -192,7 +192,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/8.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/8.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Lightning Wings</h5>
                                     <p className="card-text ">
@@ -215,7 +215,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/9.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/9.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Ice Wings</h5>
                                     <p className="card-text ">
@@ -238,7 +238,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/10.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/10.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Blade Wings</h5>
                                     <p className="card-text ">
@@ -261,7 +261,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/11.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/11.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Fire Wings</h5>
                                     <p className="card-text ">
@@ -284,7 +284,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/12.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/12.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Blazing Fire Wings</h5>
                                     <p className="card-text ">
@@ -307,7 +307,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/13.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/13.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Archdaemon Wings</h5>
                                     <p className="card-text ">
@@ -330,7 +330,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/14.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/14.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Onyx Wings</h5>
                                     <p className="card-text ">
@@ -353,7 +353,7 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/15.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/15.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Frosty Ice Wings</h5>
                                     <p className="card-text ">
@@ -376,11 +376,35 @@ export default function Wings() {
                     <div className="card">
                         <div className="flip-card-inner2 ">
                             <div className="flip-card-front">
-                                <img src="asset/wings/16.png" className="card-img-top mx-auto pt-1 " alt="..." />
+                                <img src="asset/wings/16.png" className="card-img-top mx-auto pt-4 " alt="..." />
                                 <div className="card-body ">
                                     <h5 className="card-title nosvoid-color ">Crystal Wings</h5>
                                     <p className="card-text ">
                                         <h5>Can be obtained from Act4 Craft NPC and Void Mall.</h5>
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="mt-3">
+                <div className="card-group   ">
+                    <div className="card">
+                        <div className="flip-card-inner2 ">
+                            <div className="flip-card-front">
+                                <img src="asset/wings/20.png" className="card-img-top mx-auto pt-4 " alt="..." />
+                                <div className="card-body ">
+                                    <h5 className="card-title nosvoid-color ">Lunar Wings</h5>
+                                    <p className="card-text ">
+                                        <h5>Can be obtained from Void Mall.</h5>
 
                                     </p>
                                 </div>
