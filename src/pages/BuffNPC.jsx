88224b44da2c999ec29dbca4109853921875edf4
Blauker <giyuudev.contact@gmail.com>
2022-04-07 03:16:58 +0200

    Buff NPC

--- src/pages/BuffNPC.jsx
+++ src/pages/BuffNPC.jsx
@@ -7,10 +7,44 @@ export default function Buff() {
 
                 <h1 className="nosvoid-color d-flex justify-content-center">Buff NPC Guide:</h1>
                 <br />
-
+                <div className=''>
+                    <div className='borderoLP'>
+                        <h3 className='d-flex justify-content-center'><b>Prestige 7:&nbsp;</b> <i>100kk</i></h3>
+                        <d className='d-flex justify-content-center'>
+                            Increases champion experience received by 30%.&nbsp;<i>Duration: 50 min</i>
+                        </d>
+                        <br />
+                        <div className='d-flex justify-content-center'>
+                            <img src="asset/BuffNPC/p7buff.gif" alt="" />
+                        </div>
+                    </div>
+                </div>
                 <br />
-                <h3>C60 Equipment Craft Scroll:</h3>
-                - Heart of Belial 10x : Can be obtained from Color PvP winner side
+                <div className=''>
+                    <div className='borderoLP'>
+                        <h3 className='d-flex justify-content-center'><b>Prestige 8:&nbsp;</b> <i>100kk</i></h3>
+                        <d className='d-flex justify-content-center'>
+                            Increases champion experience received by 40%.&nbsp;<i>Duration: 50 min</i>
+                        </d>
+                        <br />
+                        <div className='d-flex justify-content-center'>
+                            <img src="asset/BuffNPC/p8buff.gif" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div className=''>
+                    <div className='borderoLP'>
+                        <h3 className='d-flex justify-content-center'><b>Act 4:&nbsp;</b> <i>50kk</i></h3>
+                        <d className='d-flex justify-content-center'>
+                            When you finish Act4 and Caligor Raid you have 7% chance to get aditional box.&nbsp;<i>Duration: 50 min</i>
+                        </d>
+                        <br />
+                        <div className='d-flex justify-content-center'>
+                            <img src="asset/BuffNPC/a4buff.gif" alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
