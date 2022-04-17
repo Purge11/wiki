d177771709f09376c33559d4e751dae99d5446eb
Blauker <giyuudev.contact@gmail.com>
2022-04-17 18:43:39 +0200

    Costumes (Knight)

--- src/pages/Costiumes.jsx
+++ src/pages/Costiumes.jsx
@@ -896,7 +896,7 @@ export default function Costiumes() {
                                     Costume Wings:
                                     <hr />
                                     <d className="item-effect-shell">
-                                        - Overall SL increased by 3.
+                                        - Your specialist's overall skill points are increased by 3.
                                     </d>
                                     <br />
                                     <d className="item-effect-shell">
@@ -1486,7 +1486,7 @@ export default function Costiumes() {
                                     <d className="item-effect-shell">
                                         - All attacks are increased by 85
                                         <br />
-                                        - Maximum HP is increased by 2000.
+                                        - Your specialist's overall skill points are increased by 3.
                                         <br />
                                         - There is a 4% chance of causing '' Last Hope  '' on attack.
                                     </d>
@@ -1494,9 +1494,7 @@ export default function Costiumes() {
                                     <d className="text-success">
                                         Last Hope Buff:
                                         <br />
-                                        - Even if all HP are used 1 HP will remain.
-                                        <br />
-                                        - All animalus under level 99 are increased by 20%.
+                                        - Up to level 5 there is a 25% chance of never getting a bad effect.
                                     </d>
                                 </thead>
                             </table>
