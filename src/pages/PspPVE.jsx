52ff2ec31cfa27b06b147295aa150fe83f308af1
Blauker <giyuudev.contact@gmail.com>
2022-04-26 21:02:02 +0200

    Test

--- src/pages/PspPVE.jsx
+++ src/pages/PspPVE.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function pspPvE() {
     return (
         <div className='raid'>
-            <h1 className="nosvoid-color d-flex justify-content-center">Partners for PvP</h1>
+            <h1 className="nosvoid-color d-flex justify-content-center">Partners for PvE</h1>
             <img src="asset/maintence.jpg" alt="" />
         </div>
     )
