034ffabea0de30080d50d08224080fa16d35e896
Blauker <giyuudev.contact@gmail.com>
2022-04-26 20:19:12 +0200

    Pets and Partners Guides (PvP PvE and Fish) on maintenance

--- src/pages/PspFish.jsx
+++ src/pages/PspFish.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function pspFish() {
     return (
         <div className='raid'>
-            
+            <h1 className="nosvoid-color d-flex justify-content-center">Partners for PvP</h1>
+            <img src="asset/maintence.jpg" alt="" />
         </div>
     )
 }
--- src/pages/PspPVE.jsx
+++ src/pages/PspPVE.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default function pspPvE() {
     return (
         <div className='raid'>
+            <h1 className="nosvoid-color d-flex justify-content-center">Partners for PvP</h1>
+            <img src="asset/maintence.jpg" alt="" />
         </div>
     )
 }
