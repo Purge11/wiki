846be6e11f2ca4722bc7b06447a7c9ebf39530b2
GayBoysAdmirer <ermar123@interia.pl>
2022-04-26 06:36:47 +0200

    Perfection update

--- src/pages/Perfection.jsx
+++ src/pages/Perfection.jsx
@@ -7,69 +7,107 @@ export default function Perfection() {
             <div className="container">
                 <div className="row  ">
 
-                    <h4 className=" nosvoid-color d-flex justify-content-center">What is perfection stone?</h4>
-                    <br />
-
-                    Excellence gives statistics and has nothing to do with skill point distribution.
-                    <br />
-                    1 attack perf = +10 base damage (for both weapons)
-                    <br />
-                    1 def perf = +10 base def (melee, long range and magic)
-                    <br />
-                    1 element perf = +1% power of the fairy
-                    <br />
-                    1 hp/mp perf = +100 hp/mp
-                    <br />
-                    <br />
-                    Where can I get the perfection stones?
-                    <br />
-                    <br />
-                    <div>
-                        1-) It can be obtained with the Meteor mini-game. (The number of stones per
+                <div className='borderoLP'>  <p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h1>Perfection</h1></div></p>
+                <br></br><img src="asset/perfection/perfection4.png" alt="" />
+                <br></br>
+                  <p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h7>Bonuses</h7></div></p>
+                 
+<ul class="list-group">
+  <li class="list-group-item">+1 Attack PP = +10 base dmg (both weapons)</li>
+  <li class="list-group-item">+1 Defence PP = +10 base damage</li>
+  <li class="list-group-item">+1 Element PP = +1% power of the fairy</li>
+  <li class="list-group-item">+1 HP PP = +100 HP/MP</li></ul>
+<br></br>
+<br></br>
+<p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h7>How to get Perfection</h7></div></p>
+<ol class="list-group list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Meteor Game</div>
+      It can be obtained with the Meteor mini-game. (The number of stones per
                         round increases.) For Meteor mini game hours
                         <a href="schedule">《🔔》schedule</a>
-                    </div>
-                    <br />
-                    2-) It can be obtained by raiding Maru. From the Maru raid box "Perfection
-                    stone"(sum of x300 Perfection stone.) Type $Warp Raidarea to go to the Maru raid
-                    area.
-                    <br />
-                    <br />
-                    3-)Act4 map boss comes out of the "Glacerus random box" dropped from mini
-                    caligor.
-                    <br />
-                    <br />
-                    4-) You can get the perfection stones box by fishing.
-                    <br />
-                    <br />
-                    5-) You can get the SP9 perfection stones from the prestige 8 map ($Warp p8)
-                    <br />
-                    <br />
-                    How ​​can I transform perfection stones into "Perfection Stones"?
-                    <br />
-                    <br />
-                    1-) You can do it from the Perfection portals in the village of NosVille.
-                    <br />
-                    <br />
-                    How ​​to chance "Perfection Stone" into Perfection stones?
-                    <br />
-                    <br />
-                    -You can do it with the npc waiting outside the perfection portals in NosVille.
-                    <br />
-                    <br />
-                    What is auto perfection?
-                    <br />
-                    What does it do?
-                    <br />
-                    1-) You can get it from Auto Perfection NPC in NosVille.
-                    <br />
-                    2-) It helps you to print perfection on your card automatically and quickly.
-                    (For perfection, you can buy from Alchemist NPC).
-                    <br />
-                    <br />
-                    <img src="asset/perfection/perfection1.png" alt="" />
-                    <br />
-                    <img src="asset/perfection/perfection2.png" alt="" />
+    </div>
+    <span class="badge bg-primary rounded-pill"></span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Maru Raid</div>
+      It can be obtained by raiding Maru. From the Maru raid box "Perfection stone x3"(1 Perfection stone = 300 perfection gems). $Warp Raidarea to go to the Maru raid area.
+
+    </div>
+    <span class="badge bg-primary rounded-pill"></span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Glacernon Random Box</div>
+      Glacernon Random Box can be obtained from "Yeti" monster in A4. You have a chance to get Perfection stone from box.
+      
+      
+    </div>
+    
+    
+    
+    
+    
+    <span class="badge bg-primary rounded-pill"></span>
+    
+  </li>
+  <ol class="list-group list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Fishing</div>
+      You can get Perfection Boxes from fishing.
+    </div>
+    <span class="badge bg-primary rounded-pill"></span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">SP9 Perfection Stones</div>
+      You can get the SP9 perfection stones from the prestige 8 map
+
+    </div>
+    <span class="badge bg-primary rounded-pill"></span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Time-Space Boxes</div>
+      You can get Perfection Stone x5 from Time-Space box ($warp p8)
+      
+      
+    </div>
+    
+    
+    
+    
+    
+    <span class="badge bg-primary rounded-pill"></span>
+    
+  </li>
+</ol>
+</ol>
+<br></br>
+<br></br>
+<p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h7>Perfection Stones exchange</h7></div></p>
+<strong>Perfection Stone to Perfection Gems</strong><br></br>
+You can do it with the npc waiting outside the perfection portals in NosVille. <br></br>
+1x Perfection = 300x Perfection Gems
+<br></br>
+<br></br>
+<strong>Perfection Gems to Perfection Stone</strong> <br></br>
+You can do it from the Perfection portals in the village of NosVille.<br></br>
+300x Perfection Gems = 1x Perfection
+<img src="asset/perfection/perfection3.png" alt="" />
+<br></br>
+<br></br>
+<p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h7>Auto Perfection</h7></div></p>
+Auto Perfection raises your perfection by using up all yours Perfection Gems. You can get it from Auto Perfection NPC in NosVille.
+<img src="asset/perfection/perfection2.png" alt="" />
+<strong>How to use it:<br></br></strong>
+Use Auto Perfection and drag to any SP.
+</div>
+
+
 
                 </div>
             </div>
