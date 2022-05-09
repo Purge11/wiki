b3f8456c8bd48b04ee57b3c6ea7bc78c060b8536
Blauker <giyuudev.contact@gmail.com>
2022-05-09 10:05:08 +0200

    Sellable item, dark mode

--- src/pages/SellableItems.jsx
+++ src/pages/SellableItems.jsx
@@ -10,104 +10,106 @@ export default function SellableItems() {
                 <br />
                 <br />
 
-                <table className="table nosvoid-color">
-                    <thead className="thead-dark">
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="black-color">
-                            <th scope="row danger">Fernon's Energy</th>
-                            <td>80.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Timespace Master</th>
-                            <td>50.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">C50 equipment</th>
-                            <td>30.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Equipment Crystal (bow, sword, dagger...)</th>
-                            <td>30.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Jewel Crystal</th>
-                            <td>30.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Soul of Zenas</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Soul of Erenia</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Soul of Fernon</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Soul of Caligor</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Ancient equipment (bow, sword, dagger...)</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Archer's Aura</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Mage's Aura</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Martial's Aura</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Swordman's Aura</th>
-                            <td>7.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Azrael Mask</th>
-                            <td>5.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Fernon's Gloves and Shoes</th>
-                            <td>5.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Iron Key</th>
-                            <td>5.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Sealed Heavenly Gloves and Shoes</th>
-                            <td>4.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Sealed Hellord Gloves and Shoes</th>
-                            <td>4.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Erenia's Jewels (Necklace, Bracelet, Ring)</th>
-                            <td>2.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Zenas' Jewels (Necklace, Bracelet, Ring)</th>
-                            <td>2.000.000 gold.</td>
-                        </tr>
-                        <tr className="black-color">
-                            <th scope="row danger">Ancient Orc Statue</th>
-                            <td>1.000.000 gold.</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className='borderoLP'>
+                    <table className="table">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row danger">Fernon's Energy</th>
+                                <td>80.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Timespace Master</th>
+                                <td>50.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">C50 equipment</th>
+                                <td>30.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Equipment Crystal (bow, sword, dagger...)</th>
+                                <td>30.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Jewel Crystal</th>
+                                <td>30.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Soul of Zenas</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Soul of Erenia</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Soul of Fernon</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Soul of Caligor</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Ancient equipment (bow, sword, dagger...)</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Archer's Aura</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Mage's Aura</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Martial's Aura</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Swordman's Aura</th>
+                                <td>7.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Azrael Mask</th>
+                                <td>5.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Fernon's Gloves and Shoes</th>
+                                <td>5.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Iron Key</th>
+                                <td>5.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Sealed Heavenly Gloves and Shoes</th>
+                                <td>4.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Sealed Hellord Gloves and Shoes</th>
+                                <td>4.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Erenia's Jewels (Necklace, Bracelet, Ring)</th>
+                                <td>2.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Zenas' Jewels (Necklace, Bracelet, Ring)</th>
+                                <td>2.000.000 gold.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row danger">Ancient Orc Statue</th>
+                                <td>1.000.000 gold.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
