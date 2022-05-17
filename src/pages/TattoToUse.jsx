b67d2b335344bf263066ff12399d5be9ed8e6f4d
Purge <65049931+Purge11@users.noreply.github.com>
2022-05-17 16:59:45 +0300

    new png

--- src/pages/TattoToUse.jsx
+++ src/pages/TattoToUse.jsx
@@ -1,11 +1,90 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react'
 
-export default function TattoToUse() {
+export default function Shopupdate() {
     return (
-        <div className='tatto'>
-            <h1 className="nosvoid-color d-flex justify-content-center">What Tattoo should I use?</h1>
-            <img src="asset/maintence.jpg" alt="" />
+        <div className='shop'>
+            <div className="container">
+                <div className="row series-image-list ">
+
+                <div className='borderoLP'>  <p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h1>What tattoo should i use?</h1></div></p>
+
+Hello guys ! Here you can see best tattoos for each classes.<br></br>
+<strong>IMPORTANT: Different than the official servers, our tattoo CD's are different!</strong><br></br>
+<br></br>
+</div>
+<div className='borderoLP'>
+<p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h5>Mage</h5></div></p>
+First Tattoo :  <br></br>
+<div class="text-danger"> - Lion pattern: Attack Stance (Good PVE and PVP)<br></br>
+Second Tattoo : <br></br></div>
+<div class="text-danger"></div>Snake pattern: Arcane wisdom (Good PVE and PVP)<br></br>
+<br></br>
+Information of buffs:
+<br></br>
+<br></br>
+<figure class="figure">
+  <img src="asset/shells/tattoo1.png" class="figure-img img-fluid rounded" alt="A generic square placeholder with rounded corners in a figure."/>
+  <figcaption class="figure-caption"></figcaption>
+</figure>
+
+</div>
+<br></br>
+<br></br>
+<div className='borderoLP'>
+<p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h5>Swordsman</h5></div></p>
+First Tattoo :  <br></br>
+<div class="text-danger"> - Eagle pattern: Piercing Gaze (Good PVE and PVP)<br></br>
+Second Tattoo : <br></br></div>
+<div class="text-danger"></div>- Snake pattern: Arcane wisdom (Good PVE and PVP)<br></br>
+<br></br>
+Information of buffs:
+<br></br>
+<br></br>
+<figure class="figure">
+  <img src="asset/shells/tattoo2.png" class="figure-img img-fluid rounded" alt="A generic square placeholder with rounded corners in a figure."/>
+  <figcaption class="figure-caption"></figcaption>
+</figure>
+
+</div>
+<br></br>
+<br></br>
+<div className='borderoLP'>
+<p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h5>Archer</h5></div></p>
+First Tattoo :  <br></br>
+<div class="text-danger"> - Eagle pattern: Piercing Gaze (Good PVE and PVP)<br></br>
+Second Tattoo : <br></br></div>
+<div class="text-danger"></div>- Snake pattern: Arcane wisdom (Good PVE and PVP)<br></br>
+<br></br>
+Information of buffs:
+<br></br>
+<br></br>
+<figure class="figure">
+  <img src="asset/shells/tattoo2.png" class="figure-img img-fluid rounded" alt="A generic square placeholder with rounded corners in a figure."/>
+  <figcaption class="figure-caption"></figcaption>
+</figure>
+
+</div>
+<br></br>
+<br></br>
+<div className='borderoLP'>
+<p class="text-center"><div class="p-3 mb-2 bg-warning text-dark borderoLP table-void text-white"><h5>Martial Artist</h5></div></p>
+First Tattoo :  <br></br>
+<div class="text-danger"> - Eagle pattern: Piercing Gaze (Good PVE and PVP)<br></br>
+Second Tattoo : <br></br></div>
+<div class="text-danger"></div>- Snake pattern: Arcane wisdom (Good PVE and PVP)<br></br>
+<br></br>
+Information of buffs:
+<br></br>
+<br></br>
+<figure class="figure">
+  <img src="asset/shells/tattoo2.png" class="figure-img img-fluid rounded" alt="A generic square placeholder with rounded corners in a figure."/>
+  <figcaption class="figure-caption"></figcaption>
+</figure>
+</div>
+
+
+                </div>
+            </div>
         </div>
     )
 }
