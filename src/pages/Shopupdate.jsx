50a54bff72495a949d66c6bebd91db8919e17b52
Purge <65049931+Purge11@users.noreply.github.com>
2022-06-10 13:02:50 +0300

    Update Shopupdate.jsx

--- src/pages/Shopupdate.jsx
+++ src/pages/Shopupdate.jsx
@@ -7,7 +7,7 @@ export default function Shopupdate() {
                 <div className="row series-image-list ">
                     Hello NosVoid players,
                     <br />
-                    New Fortune Wheel update is alive. [3.06.2022]
+                    New Fortune Wheel update is alive. [10.06.2022]
                     <br />
                     <br />
                     <h3>What can i get from the Fortune Wheel?</h3>
@@ -52,18 +52,15 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Koala Pet</th>
+                                    <th scope="col">Shadow King's Helmet</th>
+                                    <th scope="col"><img src="asset/shop/shadowhelmet.png" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - When raid is finished, there's a 5% chance that box drops second time.
+                                    - Damage in Raids in increased by 8%.
                                     <br />
-                                    - When you finish Act4 and Caligor Raid you have 10% chance to get additional box.
+                                    - All elemental resistance is increased by 7.
                                     <br />
-                                   - Increases fame received by 15%.
-                                   <br />
-                                   - Increases Gold earned by 5%.
-                                   <br />
-                                   - Damage in Raids is increased by 5%.
+                                   - The chance of receiving a critical hit is increased by 3%.
                                 </d>
                             </thead>
                         </table>
@@ -73,14 +70,13 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Freya (S Buff)</th>
-                                    <th scope="col"><img src="asset/shop/freya.png" alt="" /></th>
+                                    <th scope="col">Shadow King's Mask</th>
+                                    <th scope="col"><img src="asset/shop/shadowmask.png" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - Magic attacks are increased by 12%<br />
-                                    - All defence powers are increased by 80.<br />
-                                    - Movement speed is increased by 1.<br />
-                                    - Increases damage in Christmas Raid by 5%.
+                                    - Damage to all monsters is increased by 7%.<br />
+                                    - Damage in Raids is increased by 10%.<br />
+                                    - Increases Gold earned by 7%.
                                 </d>
                             </thead>
                         </table>
@@ -91,13 +87,13 @@ export default function Shopupdate() {
                         <table className="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Barni (S Buff)</th>
-                                    <th scope="col"><img src="asset/shop/barni.webp" alt="" /></th>
+                                    <th scope="col">Aegir (S Buff)</th>
+                                    <th scope="col"><img src="asset/shop/aegir.webp" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    - Ranged attacks are increased by 12%<br />
-                                    - Increases damage from critical hits by 15%.<br />
-                                    - Dodge is increased by 80.<br />
+                                    - Meele attacks are increased by 12%<br />
+                                    - All defence powers are increased by 150.<br />
+                                    - Hit rate of all attacks is increased by 80.<br />
                                     - Increases damage in Christmas Raid by 5%.
                                 </d>
                             </thead>
@@ -108,31 +104,16 @@ export default function Shopupdate() {
 
                     <div className='mt-5'>
                         <table className="table">
-                            <thead>
+                        <thead>
                                 <tr>
-                                    <th scope="col">Knight Costume Set</th>
-                                    <th scope="col"><img src="asset/shop/KnightCostume.png" alt="" /></th>
+                                    <th scope="col">Mad March (S Buff)</th>
+                                    <th scope="col"><img src="asset/EasterEvent/march.png" alt="" /></th>
                                 </tr>
                                 <d className="item-effect-shell">
-                                    Hat:<br />
-                                    - All attacks are increased by 6%.<br />
-                                    - Hit rate of all attacks is increased by 100.<br />
-                                    - Chance of inflicting critical hits is increased by 3%.<br />
-                                    <br />
-                                    Costume:<br />
-                                    - All defences are increased by 8%.<br />
-                                    - Movement Speed is increased by 1.<br />
-                                    - Up to level 4 there is a 8% chance of never getting a bad effect.<br />
-                                    <br />
-                                    Costume Wings:<br />
-                                    - All attacks are increased by 85.<br />
-                                    - Your specialist's overall skill points are increased by 3.<br />
-                                    - There is a 4% chance of causing Last Hope.<br />
-                                    <br />
-                                    < d className="text-success">
-                                    Last Hope :<br />
-                                    - Up to level 5 there is a 25% chance of never getting a bad effect.
-                                    </d>
+                                    - There is 5% chance that picked up gold will be doubled.<br />
+                                    - Increases Gold earned by 20%.<br />
+                                    - Damage to all monsters is increased by 20.<br />
+                                    - Movement speed is increased by 1.
                                 </d>
                             </thead>
                         </table>
